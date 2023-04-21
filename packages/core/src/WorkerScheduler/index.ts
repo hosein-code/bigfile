f9d32271fb51818ce6636704a172e4daf11150fb
hosein <personal_sh@163.com>
2023-04-21 16:16:41 +0800

    🎨 代码简化

--- packages/core/src/WorkerScheduler/index.ts
+++ packages/core/src/WorkerScheduler/index.ts
@@ -1,18 +1,9 @@
 import { execWorker } from "./Worker";
-import { FileChunk, FileSplitData } from './workers/FileSplit.worker'
+import { FileChunk, FileSplitData } from "./workers/FileSplit.worker";
 
 export const splitFile = (data: FileSplitData) => {
   return new Promise<FileChunk[]>((resovle, reject) => {
-    execWorker<FileSplitData, FileChunk[]>(
-      "SPLIT_FILE",
-      data,
-      (data) => {
-        resovle(data);
-      },
-      (error) => {
-        reject(error);
-      }
-    );
+    execWorker<FileSplitData, FileChunk[]>("SPLIT_FILE", data, resovle, reject);
   });
 };
 
